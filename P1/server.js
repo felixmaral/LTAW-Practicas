dbd0771d51066d41c3d4894ce9755115da06011b
Felix <felixmaral131@gmail.com>
2024-02-20 18:44:25 +0100

    Avances p2

--- P1/server.js
+++ P1/server.js
@@ -4,54 +4,95 @@ const http = require('http');
 const PUERTO = 9090;
 
 //-- Crear el servidor
-const server = http.createServer((req, res) => {
-    
-  //-- Indicamos que se ha recibido una petición
-  console.log("Petición recibida!");
+const fs = require('fs');
 
-  //-- Cabecera que indica el tipo de datos del
-  //-- cuerpo de la respuesta: HTML
-  res.setHeader('Content-Type', 'text/html');
+const TIENDA_INDEX = "index.html";
+const TIENDA_CSS = "index.css";
 
-  //-- Mensaje del cuerpo
-  res.write(`
-  <!DOCTYPE html>
+const pagina_main = `
+
+<!DOCTYPE html>
 <html lang="es">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!-- Enlaces a tus scripts de JavaScript aquí -->
+    <link rel="stylesheet" href="index.css">
+    <title>Instrumentos Musicales</title>
 </head>
 <body>
-
     <header>
-        <h1>FMusic</h1>
-        <!-- Barra de navegación -->
-        <nav>
-            <ul>
-                <li><a href="#">Inicio</a></li>
-                <li><a href="#">Productos</a></li>
-                <li><a href="#">Carrito</a></li>
-                <li><a href="#">Contacto</a></li>
-            </ul>
-        </nav>
+        <h1>Instrumentos Musicales</h1>
     </header>
-
-    <main>
-        <!-- Contenido principal de tu tienda, como productos, categorías, etc. -->
-    </main>
-
+    <section class="products">
+        <article class="product">
+            <img src="violin_bernd_hiller.jpeg" alt="violin_bernd_hiller">
+            <h2>    Violín 4/4 - Bernd Hiller</h2>
+            <p>Descripción del producto 1</p>
+            <span class="price">$ 9555</span>
+            <button>Añadir al carrito</button>
+        </article>
+        <article class="product">
+            <img src="guitarra_electrica_red.jpeg" alt="Producto 2">
+            <h2>Producto 2</h2>
+            <p>Descripción del producto 2.</p>
+            <span class="price">$24.99</span>
+            <button>Añadir al carrito</button>
+        </article>
+        <!-- Agrega más productos según sea necesario -->
+    </section>
     <footer>
-        <!-- Pie de página con información adicional y enlaces útiles -->
-        <p>&copy; 2024 FMusic</p>
+        <p>&copy; 2024 Instrumentos Musicales</p>
     </footer>
 </body>
 </html>
-`);
 
-  //-- Terminar la respuesta y enviarla
-  res.end();
+`
 
+const pagina_error = `
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mi tienda</title>
+</head>
+<body style="background-color: red">
+    <h1 style="color: white">ERROR!!!!</h1>
+</body>
+</html>
+
+`;
+
+const server = http.createServer((req, res)=>{
+    console.log("Petición recibida!");
+
+    //-- Valores de la respuesta por defecto
+    let code = 200;
+    let code_msg = "OK";
+    let page = pagina_main;
+
+    //-- Analizar el recurso
+    //-- Construir el objeto url con la url de la solicitud
+    const url = new URL(req.url, 'http://' + req.headers['host']);
+    console.log(url.pathname);
+
+    //-- Cualquier recurso que no sea la página principal
+    //-- genera un error
+    //if (url.pathname != '/') {
+    //    code = 404;
+    //    code_msg = "Not Found";
+    //   page = pagina_error;
+    //}
+
+    //-- Generar la respusta en función de las variables
+    //-- code, code_msg y page
+    res.statusCode = code;
+    res.statusMessage = code_msg;
+    res.setHeader('Content-Type','text/html');
+    res.write(page);
+    res.end();
 });
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
