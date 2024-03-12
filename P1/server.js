f6e0778e6e76cc6f69c85f842fb0c2490c3712ee
Felix <felixmaral131@gmail.com>
2024-03-12 10:22:03 +0100

    añadiendo lectura de recursos

--- P1/server.js
+++ P1/server.js
@@ -7,42 +7,79 @@ const PUERTO = 9090;
 
 const pagina_main = 'index.html'
 const pagina_error = 'error.html'
+const css = 'index.css'
 
-fs.readFile(pagina_main, 'utf8', (err, data_index) => {
-    if (err) {
-        console.log('Error al leer el archivo')
-    }
-    console.log('Archivo "' + pagina_main + '" leido')
-});
+
 
 const server = http.createServer((req, res)=>{
     console.log("Petición recibida");
-
-    //-- Valores de la respuesta por defecto (Ok)
-    let code = 200;
-    let code_msg = "OK";
-    let page = data;
 
     //-- Analizar el recurso
     //-- Construir el objeto url con la url de la solicitud
     const url = new URL(req.url, 'http://' + req.headers['host']);
     console.log('Ruta: ' + url.pathname);
 
-    //-- Cualquier recurso que no sea la página principal
-    //-- genera un error
-    if (url.pathname != '/') {
-        code = 404;
-        code_msg = "Not Found";
-        page = pagina_error;
+
+    if (url.pathname == '/' || url.pathname.endsWith(".html")) {
+        recurso = url.pathname.substring(1)
+        code = 200;
+        code_msg = "Ok";
+        fs.readFile(pagina_main, 'utf8', (err, data_index) => {
+            if (err) {
+                console.log('Error al leer el archivo')
+            }
+            console.log('Archivo "' + pagina_main + '" leido')
+            page = data_index;
+            
+            res.statusCode = code;
+            res.statusMessage = code_msg;
+            res.setHeader('Content-Type','text/html');
+            res.write(data_index);
+            res.end();
+        });
+    }
+
+    else if (url.pathname.endsWith('.css')) {
+        recurso = url.pathname.substring(1)
+        code = 200;
+        code_msg = "Ok";
+        fs.readFile(recurso, 'utf8', (err, data_index) => {
+            if (err) {
+                console.log('Error al leer el archivo')
+            }
+            console.log('Archivo "' + recurso + '" leido')
+            page = data_index;
+            
+            res.statusCode = code;
+            res.statusMessage = code_msg;
+            res.setHeader('Content-Type','text/css');
+            res.write(data_index);
+            res.end();
+        });
+    }
+
+    else if (url.pathname.endsWith('.jpeg')) {
+        recurso = url.pathname.substring(1)
+        code = 200;
+        code_msg = "Ok";
+        fs.readFile(recurso, 'utf8', (err, data_index) => {
+            if (err) {
+                console.log('Error al leer el archivo')
+            }
+            console.log('Archivo "' + recurso + '" leido')
+            page = data_index;
+            
+            res.statusCode = code;
+            res.statusMessage = code_msg;
+            res.setHeader('Content-Type','text/jpeg');
+            res.write(data_index);
+            res.end();
+        });
     }
 
     //-- Generar la respusta en función de las variables
     //-- code, code_msg y page
-    res.statusCode = code;
-    res.statusMessage = code_msg;
-    res.setHeader('Content-Type','text/html');
-    res.write(page);
-    res.end();
+    
 });
 
 //-- Activar el servidor: ¡Que empiece la fiesta!
