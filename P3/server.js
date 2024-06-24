0d45a0eee7c85a4036ba26ac2b9e94419249bf9c
Félix Martínez <felixmaral131@gmail.com>
2024-06-24 20:06:21 +0200

    avances

--- P3/server.js
+++ P3/server.js
@@ -13,48 +13,21 @@ app.use(express.static(__dirname));
 io.on('connection', (socket) => {
   console.log('Nuevo usuario conectado');
 
-  // Enviar mensaje de bienvenida solo al usuario que se conecta
-  socket.emit('message', 'Bienvenido al chat!');
+  socket.on('join', (nickname) => {
+    socket.nickname = nickname;
+    socket.broadcast.emit('message', { nickname: 'Servidor', message: `${nickname} se ha unido al chat` });
+  });
 
-  // Anunciar a todos los usuarios cuando alguien se conecta
-  socket.broadcast.emit('message', 'Un nuevo usuario se ha conectado');
-
-  // Manejar mensajes del cliente
   socket.on('message', (message) => {
-    if (message.startsWith('/')) {
-      // Procesar comandos especiales
-      handleCommand(socket, message);
-    } else {
-      // Reenviar mensaje a todos los usuarios
-      io.emit('message', message);
+    io.emit('message', { nickname: socket.nickname, message });
+  });
+
+  socket.on('disconnect', () => {
+    if (socket.nickname) {
+      io.emit('message', { nickname: 'Servidor', message: `${socket.nickname} se ha desconectado` });
     }
   });
-
-  // Manejar desconexión de un usuario
-  socket.on('disconnect', () => {
-    io.emit('message', 'Un usuario se ha desconectado');
-  });
 });
-
-// Función para manejar comandos especiales
-function handleCommand(socket, command) {
-  switch (command) {
-    case '/help':
-      socket.emit('message', 'Comandos disponibles: /help, /list, /hello, /date');
-      break;
-    case '/list':
-      socket.emit('message', `Usuarios conectados: ${io.engine.clientsCount}`);
-      break;
-    case '/hello':
-      socket.emit('message', '¡Hola! Bienvenido al chat');
-      break;
-    case '/date':
-      socket.emit('message', `La fecha actual es: ${new Date().toLocaleDateString()}`);
-      break;
-    default:
-      socket.emit('message', 'Comando no reconocido');
-  }
-}
 
 server.listen(PORT, () => {
   console.log(`Servidor escuchando en el puerto ${PORT}`);
