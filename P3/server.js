8fcf32d5feffcb8e1b0e4f57d39f9925ee57f409
Félix Martínez <felixmaral131@gmail.com>
2024-06-24 20:30:44 +0200

    FUNCIONAL BÁSICO

--- P3/server.js
+++ P3/server.js
@@ -15,18 +15,51 @@ io.on('connection', (socket) => {
 
   socket.on('join', (nickname) => {
     socket.nickname = nickname;
+    socket.emit('message', { nickname: 'Servidor', message: `¡Bienvenido al chat, ${nickname}!` });
     socket.broadcast.emit('message', { nickname: 'Servidor', message: `${nickname} se ha unido al chat` });
   });
 
   socket.on('message', (message) => {
-    io.emit('message', { nickname: socket.nickname, message });
+    if (message.startsWith('/')) {
+      handleCommand(message);
+    } else {
+      broadcastMessage(`${socket.nickname}`, message);
+    }
   });
 
   socket.on('disconnect', () => {
     if (socket.nickname) {
-      io.emit('message', { nickname: 'Servidor', message: `${socket.nickname} se ha desconectado` });
+      broadcastMessage('Servidor', `${socket.nickname} se ha desconectado`);
     }
   });
+
+  const broadcastMessage = (sender, message) => {
+    io.emit('message', { nickname: sender, message });
+  };
+
+  const handleCommand = (message) => {
+    const command = message.substring(1).trim(); // Eliminar el '/' y espacios en blanco
+
+    switch (command) {
+      case 'help':
+        socket.emit('message', { nickname: 'Servidor', message: `Comandos disponibles: /help, /list, /hello, /date` });
+        break;
+      case 'list':
+        const connectedUsers = Object.keys(io.sockets.sockets).length;
+        socket.emit('message', { nickname: 'Servidor', message: `Usuarios conectados: ${connectedUsers}` });
+        break;
+      case 'hello':
+        socket.emit('message', { nickname: 'Servidor', message: `Hola, ${socket.nickname}!` });
+        break;
+      case 'date':
+        const currentDate = new Date().toLocaleDateString();
+        socket.emit('message', { nickname: 'Servidor', message: `Fecha actual: ${currentDate}` });
+        break;
+      default:
+        socket.emit('message', { nickname: 'Servidor', message: `Comando no reconocido. Escribe /help para ver los comandos disponibles.` });
+        break;
+    }
+  };
 });
 
 server.listen(PORT, () => {
